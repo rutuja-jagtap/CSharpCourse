e14f50507849dccd633c57798d362857822d4a5a
Jagtap Rutuja Vijay <70133640+rutuja-jagtap@users.noreply.github.com>
2022-04-01 13:27:57 +0530

    Calculate Area of Rectangle
    Taking input from user
    Calculating area of rectangle
    printing results

---
+++ RectangleArea.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBasics
+{
+    class RectangleArea
+    {
+        static double CalculateArea(double len,double wid)
+        {
+            return len * wid;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter length and bradth to calcute area of rectangle\n");
+            double len=Convert.ToDouble(Console.ReadLine());
+            double wid= Convert.ToDouble(Console.ReadLine());
+            double res = RectangleArea.CalculateArea(len, wid);
+            Console.WriteLine("Length="+len+ "\n" + "Width="+wid+"\n"+"Area of rectangle ="+res);
+        }
+    }
+}
